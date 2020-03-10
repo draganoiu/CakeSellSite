9460e9347acfa156cb74ca87243b8f62f0bd6653
andrei.draganoiu <andrei.draganoiu@certent.com>
2020-03-10 17:07:10 +0200

    Entity framework and setting up the database

--- CakeSell/Startup.cs
+++ CakeSell/Startup.cs
@@ -6,6 +6,7 @@ using CakeSell.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +18,8 @@ namespace CakeSell
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<AppContext>(options => options.UseSqlServer());
+
 			services.AddScoped<IPieRepository, MockPieRepository>();
 			services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 			services.AddControllersWithViews();
